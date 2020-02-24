79e0a27d1a54a0779064383384267b2c5dcec487
Jonas Stare <yownas@gmail.com>
2020-02-24 18:15:26 +0100

    Clean up and some checks

--- compresssed.py
+++ compresssed.py
@@ -3,52 +3,77 @@
 import sys
 import string
 
+# Read all the data
 data = sys.stdin.read()
 
 limit = 1
 sed_len = 9
+markers = list()
 orig_len = len(data)
 
-chars = string.digits + string.ascii_letters
-subs = list()
-
 # Keep chars that don't exist
+chars = string.digits + string.ascii_letters
 for c in list(chars):
-  if c not in data:
-    subs.append(c)
+    if c not in data:
+        markers.append(c)
+
+# Check that we have any markers left (or fail)
+if len(markers) <= 1:
+  print("Not enough unused chars in input to do anything useful. :(")
+  sys.exit(1)
+
+if data.find("\n") != -1:
+    c = markers.pop(0)
+    data = data.replace("\n", c)
+    sed_post = "y/" + c + "/\\n/;"
+else:
+    sed_post = ""
 
 print("We will use these chars as markers:")
-print(''.join(subs))
+print(''.join(markers))
 
-sed_pre = "s/^.* #//;"
-sed_post = " #"
+if data.find("#") != -1:
+    sed_pre = "s/^.* #" + markers[0]+ "//;"
+    sed_post += " #" + markers[0]
+else:
+    sed_pre = "s/^.* #//;"
+    sed_post += " #"
+
 sed = ""
 
-def compressed(string, mark):
-  bests = 0
-  bestp = ""
-  pats = set()
-  for i in range(1, int((len(string)-1))):
-    for j in range(2, int((len(string)-i)/2)):
-      if (i + j) <= len(string):
-        p = string[i:i+j]
-        c = string.count(p)
-        saved = (c*(len(p) - 1)) - (sed_len + len(p))
-        if saved > bests:
-          bests = saved
-          bestp = p
-  return(bestp, bests)
+def compressed(string):
+    bests = 0
+    bestp = ""
+    # Find the substring that will save the most space
+    for i in range(1, int((len(string)-2))):
+        for j in range(2, int((len(string)-i)/2)):
+            if (i + j) <= len(string):
+                p = string[i:i+j]
+                c = string.count(p)
+                saved = (c*(len(p) - 1)) - (sed_len + len(p))
+                if saved > bests:
+                    bests = saved
+                    bestp = p
+    return(bestp, bests)
 
-for s in subs:
-  (pat, saved) = compressed(data, s)
-  if saved < limit:
-    break
-  data = data.replace(pat, s)
-  pat = pat.replace("\\","\\\\")
-  sed = ("s/%s/%s/g;" % (s, pat)) + sed
-  print("Shrank: %d bytes" % (saved))
-  output = sed_pre + sed + sed_post + data
-  print(output)
+# As long as we have markers, find a substring to replace
+for m in markers:
+    (pat, saved) = compressed(data)
+    # ...actually, if we don't same enough, just give up.
+    if saved < limit:
+        break
+    data = data.replace(pat, m)
+    m = m.replace('\\', '\\\\')
+    m = m.replace('/', '\/')
+    m = m.replace('&', '\&')
+    pat = pat.replace('\\', '\\\\')
+    pat = pat.replace('/', '\/')
+    pat = pat.replace('&', '\&')
+    sed = ("s/%s/%s/g;" % (m, pat)) + sed
+    print("Shrank: %d bytes" % (saved))
+    output = sed_pre + sed + sed_post + data
+    print(output)
+    print("")
 
 print("Original length: %d" % (orig_len))
 print("Final length: %d (%d)" % (len(output), len(data)))
